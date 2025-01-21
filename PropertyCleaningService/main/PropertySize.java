ffad259845c540b04f1ffcebdd289d54328cb7d7
user-jaywyang <156152290+user-jaywyang@users.noreply.github.com>
2025-01-21 12:11:39 -0800

    Create PropertySize.java

---
+++ PropertyCleaningService/main/PropertySize.java
@@ -0,0 +1,22 @@
+package problem1;
+
+/**
+ * The PropertySize enum represents the different size categories of a property.
+ */
+public enum PropertySize {
+
+  /**
+   * Represents a small-sized property.
+   */
+  SMALL,
+
+  /**
+   * Represents a medium-sized property.
+   */
+  MEDIUM,
+
+  /**
+   * Represents a large-sized property.
+   */
+  LARGE
+}
